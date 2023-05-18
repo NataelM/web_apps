f94c04ddc4ec0c33c9ca8b001bd7c2e3e8cc7160
Jonathan Ramírez <80018161+NataelM@users.noreply.github.com>
2023-05-17 20:16:20 -0500

    agregue mas comentarios

--- 3_visualization/01_static_vis.py
+++ 3_visualization/01_static_vis.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# hola
+# hola soy jonathan
 st.header('matplotlib and searborn visualization in streamlit')
 
 # load the data
